14b0a40a8a762e91b1f997b5eeec4b39d6e11b2e
JasPradhan <70465360+JasPradhan@users.noreply.github.com>
2020-11-07 15:07:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
- 
+status="";
+
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(325,325);
+    canvas.position(600,377);
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function start(){
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+    input=document.getElementById("input").value;
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status=true;
+}
+
+function draw(){
+    image(video,0,0,325,325);
+}
